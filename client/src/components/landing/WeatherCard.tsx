87ea16653819318a50686573df0c07ffb161d68c
Maia <Maiakbard@gmail.com>
2026-05-26 13:28:06 -0500

    feat(weathercard): add loading spinner inside card and center it

--- client/src/components/landing/WeatherCard.tsx
+++ client/src/components/landing/WeatherCard.tsx
@@ -4,28 +4,32 @@ const WeatherCard = ({weatherData, isLoading }) => {
 
   return (
     <>
-      {isLoading || !weatherData ? 
-        <div className="animate-spin rounded-full h-8 w-8 border-4 border-gray-200 border-t-[#09b8d4]"></div>
-       : 
-        <div className="bg-white p-4 rounded-lg shadow-md mb-6 mt-8  w-full border-t-4 border-t-[#09b8d4] text-center">
-          <h2 className="text-xl font-bold mb-3 mt-3">{weatherData.city.locality}, {weatherData.city.principalSubdivision}</h2>
-          <h1 className="text-4xl font-bold">
-            {Math.round(weatherData.weather.current.temperature_2m)}°F
-          </h1>
-          <p className="text-gray-600">
-            Feels like {Math.round(weatherData.weather.current.apparent_temperature)}°F
-          </p>
-          <img
-            src={`https://openweathermap.org/img/wn/${getWeatherIcon(weatherData.weather.current.weather_code)}@2x.png`}
-            alt="Weather condition graphic"
-            className="w-16 h-16 object-contain mx-auto"
-            onError={(e) => {
-              // If the image server fails, fallback to a standard emoji text display
-              e.currentTarget.style.display = "none";
-            }}
-          />
-        </div>
-      }
+      <div className="bg-white p-4 rounded-lg shadow-md mb-6 mt-8  w-full border-t-4 border-t-[#09b8d4] text-center">
+        {isLoading || !weatherData ? 
+          <div className='flex items-center justify-center h-32'>
+            <div className="animate-spin rounded-full h-8 w-8 border-4 border-gray-200 border-t-[#09b8d4]"></div>
+          </div>
+        : 
+          <div>
+            <h2 className="text-xl font-bold mb-3 mt-3">{weatherData.city.locality}, {weatherData.city.principalSubdivision}</h2>
+            <h1 className="text-4xl font-bold">
+              {Math.round(weatherData.weather.current.temperature_2m)}°F
+            </h1>
+            <p className="text-gray-600">
+              Feels like {Math.round(weatherData.weather.current.apparent_temperature)}°F
+            </p>
+            <img
+              src={`https://openweathermap.org/img/wn/${getWeatherIcon(weatherData.weather.current.weather_code)}@2x.png`}
+              alt="Weather condition graphic"
+              className="w-16 h-16 object-contain mx-auto"
+              onError={(e) => {
+                // If the image server fails, fallback to a standard emoji text display
+                e.currentTarget.style.display = "none";
+              }}
+            />
+          </div>
+        }
+      </div>
     </>
   )
 }
