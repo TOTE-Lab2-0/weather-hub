b3e33f8adb420d98ed1437ca35c50cecf975be8e
Maia <Maiakbard@gmail.com>
2026-05-28 18:45:02 -0500

    fix(PR): add fixes for PR comments

--- client/src/components/shared/LoadingScreen.tsx
+++ client/src/components/shared/LoadingScreen.tsx
@@ -3,7 +3,7 @@ const LoadingScreen = () => {
   return (
     <>
       <div className='flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-[#0a1628] to-[#0d2b4e]'>
-        <div className='text-white text center'>
+        <div className='text-white text-center'>
           <div className='w-20 h-20 rounded-full bg-white/20 flex items-center justify-center text-lg'>
             <img className='w-14 h-14' src={myLogo} alt='icon'/>
           </div>
